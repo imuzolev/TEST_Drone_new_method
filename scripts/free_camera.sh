100dcc215947b5bd4747b2ec500a18f627b369c3
Ivan <50378504+imuzolev@users.noreply.github.com>
2026-07-28 18:05:00 +0300

    TC: delete scripts/free_camera.sh

--- scripts/free_camera.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Sends CameraTrack NONE to release camera from follow mode
-for i in 1 2 3 4 5; do
-    gz topic -t /gui/camera/track -m gz.msgs.CameraTrack -p "track_mode: 0"
-    sleep 0.3
-done
-echo "Sent 5 x track_mode:NONE to /gui/camera/track"
