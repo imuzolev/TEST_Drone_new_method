5f009cf64cdeb20099a3f89ad5644c44cc08729a
Ivan <50378504+imuzolev@users.noreply.github.com>
2026-07-28 18:04:59 +0300

    TC: delete scripts/follow_drone.sh

--- scripts/follow_drone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Attach camera to follow the drone
-gz topic -t /gui/camera/track -m gz.msgs.CameraTrack \
-  -p "track_mode: 2, follow_target: {name: \"x500_warehouse_0\", type: 2}, follow_offset: {x: -2, y: -2, z: 2}"
-echo "Camera follow: ON (x500_warehouse_0)"
